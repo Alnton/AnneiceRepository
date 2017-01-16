b2e0c437cf3623cdaab381bb817e19020e6912aa
Alnton <15951089194@139.com>
2017-01-16 17:06:06 +0800

    学习资料
    各种学习资料，很全面也都是我自己喜欢的材料，总结的东西。

--- 学习资料/底层学习/uc/day02/add.h
+++
@@ -1,5 +0,0 @@
-#ifndef ADD_H_
-#define ADD_H_//防止重复被导入
-int add(int,int);
-double add2(double,double);
-#endif
--- 学习资料/底层学习/uc/day07/fork1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(){
-  printf("begin%d\n",getpid());
-  pid_t pid = fork();//开始创建子进程,代码区在第6行
-  printf("end:%d\n",pid);
-}
-
